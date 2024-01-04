f0cef5d09fedcf0e50848c29fc0e8a912ac239dd
bamboo-wood <cantate.1009.0417@gmail.com>
2024-01-04 14:18:47 +0900

    Initial Commit

---
+++ KeyframeAnimation/KeyframeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyframeAnimationApp.swift
+//  KeyframeAnimation
+//
+//  Created by 佐々木一樹 on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct KeyframeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
