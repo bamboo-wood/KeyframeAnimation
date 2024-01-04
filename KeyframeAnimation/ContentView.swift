6ada7845c37b78fad8269d6485853485e6bb2cfe
bamboo-wood <cantate.1009.0417@gmail.com>
2024-01-04 15:04:40 +0900

    Add keyframe animation to ContentView

--- KeyframeAnimation/ContentView.swift
+++ KeyframeAnimation/ContentView.swift
@@ -8,15 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var startKeyframeAnimation: Bool = false
+
     var body: some View {
         VStack {
+            Spacer()
+
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .keyframeAnimator(initialValue: Keyframe(), trigger: startKeyframeAnimation) { content, value in
+                    content
+                        .scaleEffect(value.scale)
+                        .offset(y: value.offsetY)
+                        .rotationEffect(Angle(degrees: value.rotation))
+                        .opacity(value.reflectionOpacity)
+                } keyframes: { _ in
+                    KeyframeTrack(\.scale) {
+                        SpringKeyframe(1, duration: 1)
+                        SpringKeyframe(1, duration: 1, spring: .bouncy, startVelocity: 35.0)
+                        SpringKeyframe(1, duration: 1)
+                    }
+                    KeyframeTrack(\.rotation) {
+                        SpringKeyframe(0, duration: 1)
+                        SpringKeyframe(0, duration: 1)
+                        SpringKeyframe(30.0, duration: 1, spring: .bouncy, startVelocity: 35.0)
+                        SpringKeyframe(0, duration: 1)
+                    }
+                    KeyframeTrack(\.offsetY) {
+                        SpringKeyframe(-60, duration: 1)
+                        SpringKeyframe(-60, duration: 1)
+                        SpringKeyframe(-60, duration: 1)
+                        SpringKeyframe(-60, duration: 1)
+                        SpringKeyframe(0, duration: 1)
+                    }
+                }
+
+            Spacer()
+
+            Button("Start") {
+                startKeyframeAnimation.toggle()
+            }
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundStyle(.blue)
         }
-        .padding()
     }
+}
+
+struct Keyframe {
+    var scale: CGFloat = 1
+    var offsetY: CGFloat = .zero
+    var rotation: CGFloat = .zero
+    var reflectionOpacity: CGFloat = 1.0
 }
 
 #Preview {
